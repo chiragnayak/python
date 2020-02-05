568754b830647b4f38cf0ebfe5d8fb2ba9f61e45
emperorchirag@gmail.com <Winner@@123>
2020-02-05 15:48:07 +0530

    commit from m a c

--- LearningPy/concepts/dictionaryExp/dicExp.py
+++ LearningPy/concepts/dictionaryExp/dicExp.py
@@ -4,3 +4,9 @@ hi["english"]="helloWorld"
 hi['hindi']={"chirag":"nayak"}
 print(hi['hindi']['chirag'])
 print(hi)
+
+# /Users/cnayak/PycharmProjects/python/venv/bin/python /Users/cnayak/PycharmProjects/python/LearningPy/concepts/dictionaryExp/dicExp.py
+# nayak
+# {'english': 'helloWorld', 'hindi': {'chirag': 'nayak'}}
+#
+# Process finished with exit code 0
