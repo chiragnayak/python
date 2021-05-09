e8895c7691d7b2a4c53ff8e70b6261019c0c29bb
cnayakarkin <Winner@123454321>
2021-05-09 11:59:26 +0530

    cowin script

--- LearningPy/cowin_scripts/find_email_slot_by_city.py
+++ LearningPy/cowin_scripts/find_email_slot_by_city.py
@@ -36,70 +36,74 @@ if __name__ == "__main__":
         REQUIRED_DATE = X_DATE.strftime('%d-%m-%Y')
         DATES.append(REQUIRED_DATE)
 
-    for city in CITIES:
-        city_message = list()
-        display = "FOR CITY : {} ".format(city)
-        NO_SLOT_CITY = True
-        city_message.append("\n\n")
-        city_message.append("=" * 40)
-        city_message.append(display)
-        city_message.append("=" * 40)
-        print(display)
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-        request_text = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={}&date={}".format(city, TODAYS_DATE)
-        resp = requests.get(request_text, headers=headers)
+    for date in DATES:
+        for city in CITIES:
+            city_message = list()
+            display = "FOR CITY : {} ".format(city)
+            NO_SLOT_CITY = True
+            city_message.append("\n\n")
+            city_message.append("=" * 40)
+            city_message.append(display)
+            city_message.append("=" * 40)
+            print(display)
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+            request_text = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={}&date={}".format(city, date)
+            resp = requests.get(request_text, headers=headers)
 
-        if resp.status_code != 200:
-            # This means something went wrong.
-            raise Exception("API FAILED -> CODE = {}".format(resp.status_code))
+            if resp.status_code != 200:
+                # This means something went wrong.
+                raise Exception("API FAILED -> CODE = {}".format(resp.status_code))
 
 
-        response = json.loads(resp.text)
-        available = False
-
-        if len(response["sessions"]) == 0:
-            city_message.append("x" * 40)
-            city_message.append("NO CENTRE AVAILABLE FOR PINCODE {}".format(city))
-            city_message.append("x" * 40)
-            continue
-        else:
-            for session in response["sessions"]:
-                age_limit = str(session["min_age_limit"])
-                slots_available = session["available_capacity"]
-                if session["date"] in DATES and age_limit == str(AGE) and slots_available > 1:
-                    session_text = list()
-                    session_text.append("===" * 10)
-                    session_text.append("SLOT AVAILABLE")
-                    session_text.append("===" * 10)
-                    session_text.append(str(session["name"]))
-                    session_text.append("CENTER NAME : {}".format(str(session["name"])))
-                    session_text.append("ADDRESS : {}".format(str(session["address"])))
-                    session_text.append("PINCODE : {}".format(str(session["pincode"])))
-                    session_text.append("AVAILABLE : {}".format(str(session["available_capacity"])))
-                    session_text.append(str(session["vaccine"]))
-                    session_text.append(str(session["slots"]))
-                    session_text.append("\n" * 1)
-                    available = True
-                    NO_SLOT_CITY = False
-                    NO_SLOT_AT_ALL = False
-                    x = "\n".join(session_text)
-                    city_message.append(x)
-
-                if NO_SLOT_CITY:
-                    city_message.append("xxx NO SLOT AVAILABLE FOR THIS DATE xxx\n\n")
-
-        if available:
-            city_message.insert(0, "Subject: AGE: {} | PIN : {} COVID VACCINE AVAILABLE, BOOK NOW".format(AGE, city))
-            x = "\n".join(city_message)
-            send_email(x)
+            response = json.loads(resp.text)
             available = False
+
+            if len(response["sessions"]) == 0:
+                city_message.append("x" * 40)
+                city_message.append("NO CENTRE AVAILABLE FOR PINCODE {}".format(city))
+                city_message.append("x" * 40)
+                continue
+            else:
+                for session in response["sessions"]:
+                    age_limit = str(session["min_age_limit"])
+                    slots_available = session["available_capacity"]
+                    if session["date"] in DATES and age_limit == str(AGE) and slots_available > 1:
+                        session_text = list()
+                        session_text.append("===" * 10)
+                        session_text.append("SLOT AVAILABLE")
+                        session_text.append("===" * 10)
+                        session_text.append(str(session["name"]))
+                        session_text.append("CENTER NAME : {}".format(str(session["name"])))
+                        session_text.append("ADDRESS : {}".format(str(session["address"])))
+                        session_text.append("PINCODE : {}".format(str(session["pincode"])))
+                        session_text.append("AVAILABLE : {}".format(str(session["available_capacity"])))
+                        session_text.append(str(session["vaccine"]))
+                        session_text.append(str(session["slots"]))
+                        session_text.append("\n" * 1)
+                        available = True
+                        NO_SLOT_CITY = False
+                        NO_SLOT_AT_ALL = False
+                        x = "\n".join(session_text)
+                        city_message.append(x)
+
+                    if NO_SLOT_CITY:
+                        city_message.append("xxx NO SLOT AVAILABLE FOR THIS DATE xxx\n\n")
+
+            if available:
+                city_message.insert(0, "Subject: AVAILABLE | AGE: {} | PIN : {}, BOOK NOW".format(AGE, city))
+                x = "\n".join(city_message)
+                send_email(x)
+                available = False
 
     if NO_SLOT_AT_ALL:
         send_email("Subject: CITY | {} | AGE {} | NO VACCINE SLOTS AVAILABLE"
                    "\n"
                    "\n"
-                   "{}".format(TODAYS_DATE, AGE, CITIES))
+                   "{}"
+                   "\n"
+                   "\n"
+                   "{}".format(TODAYS_DATE, AGE, CITIES, DATES))
 
 
 
